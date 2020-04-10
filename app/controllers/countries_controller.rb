40c93bce96d9fa3b6c993fe749d516895e8f06a1
kennja05 <jacobkenny05@gmail.com>
2020-04-10 16:41:02 -0400

    wrote route for countries with same currency code

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,14 @@
 class CountriesController < ApplicationController
+
+    def index
+        countries = Country.all 
+        render json: countries 
+    end
+
+    def codeShow
+        code = params['code'].upcase
+        countries = Country.all.where(currency_code: code)
+        render json: countries
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :countries
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/countries/codes/:code', to: 'countries#codeShow'
+  resources :countries, only: [:index, :show]
 end
