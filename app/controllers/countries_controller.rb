d368c0fd82f89833df377abde832ee8b32cc3414
kennja05 <jacobkenny05@gmail.com>
2020-04-11 10:02:16 -0400

    added route for conversion

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,3 +1,6 @@
+require 'dotenv'
+require 'rest-client'
+
 class CountriesController < ApplicationController
 
     def index
@@ -10,5 +13,15 @@ class CountriesController < ApplicationController
         countries = Country.all.where(currency_code: code)
         render json: countries
     end
+
+    def convert
+        from = params['from']
+        to = params['to']
+        amount = params['amount']
+        byebug
+        amt = JSON.parse(RestClient.get("https://data.fixer.io/api/convert?access_key=7565a1db44ef9053cc4d95b8ac794ffc&from=GBP&to=JPY&amount=25"))
+        
+        render json: amt
+    end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/countries/codes/:code', to: 'countries#codeShow'
+  get '/countries/convert/:from/:to/:amount', to: 'countries#convert'
   resources :countries, only: [:index, :show]
 end
